a554a4b65b049367e4986e2b7fa7107f0a07f4cb
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-24 22:06:44 +0300

    feat: add user and task models

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskMasterAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
