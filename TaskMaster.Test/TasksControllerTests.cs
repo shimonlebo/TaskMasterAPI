a8db53ea0ac86033d088546a3ef420573223a070
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-25 22:43:51 +0300

    refactor: move repo and controller to private prop in TasksControllerTests

--- TaskMaster.Test/TasksControllerTests.cs
+++ TaskMaster.Test/TasksControllerTests.cs
@@ -2,24 +2,29 @@
 {
     public class TasksControllerTests
     {
+        private readonly Mock<ITaskRepository> _mockTaskRepository;
+        private readonly TasksController _controller;
+
+        public TasksControllerTests()
+        {
+            _mockTaskRepository = new Mock<ITaskRepository>();
+            _controller = new TasksController(_mockTaskRepository.Object);
+        }
+
         [Fact]
         public async void Get_ReturnsAllTasks()
         {
             // Arrange
-            var mockTastRepository = new Mock<ITaskRepository>();
-
             var tasks = new List<TaskModel>
             {
                 new TaskModel { Id = 1, Title = "Task 1" },
                 new TaskModel { Id = 2, Title = "Task 2" }
             };
 
-            mockTastRepository.Setup(repo => repo.GetTasks()).ReturnsAsync(tasks);
-
-            var controller = new TasksController(mockTastRepository.Object);
+            _mockTaskRepository.Setup(repo => repo.GetTasks()).ReturnsAsync(tasks);
 
             // Act
-            var result = await controller.GetTasks();
+            var result = await _controller.GetTasks();
             
             // Assert
             var actionResult = Assert.IsType<OkObjectResult>(result.Result);
@@ -31,16 +36,12 @@
         public async void Get_ReturnsTaskById()
         {
             // Arrange
-            var mockTastRepository = new Mock<ITaskRepository>();
-
             var task = new TaskModel { Id = 1, Title = "Task 1", IsComplete = false };
 
-            mockTastRepository.Setup(repo => repo.GetTaskById(task.Id)).ReturnsAsync(task);
-
-            var controller = new TasksController(mockTastRepository.Object);
+            _mockTaskRepository.Setup(repo => repo.GetTaskById(task.Id)).ReturnsAsync(task);
 
             // Act
-            var result = await controller.GetTaskById(task.Id);
+            var result = await _controller.GetTaskById(task.Id);
 
             // Assert
             var actionResult = Assert.IsType<OkObjectResult>(result.Result);
