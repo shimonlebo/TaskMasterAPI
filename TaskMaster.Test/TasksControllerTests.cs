c08e12f1a6a4430c877a8c41b78b13af7e62e3ee
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-25 22:18:57 +0300

    test: add unit test for GetTaskById in TasksController

--- TaskMaster.Test/TasksControllerTests.cs
+++ TaskMaster.Test/TasksControllerTests.cs
@@ -34,5 +34,26 @@ namespace TaskMaster.Test
             var model = Assert.IsType<List<TaskModel>>(actionResult.Value);
             Assert.Equal(tasks.Count, model.Count);            
         }
+
+        [Fact]
+        public async void Get_ReturnsTaskById()
+        {
+            // Arrange
+            var mockTastRepository = new Mock<ITaskRepository>();
+
+            var task = new TaskModel { Id = 1, Title = "Task 1", IsComplete = false };
+
+            mockTastRepository.Setup(repo => repo.GetTaskById(task.Id)).ReturnsAsync(task);
+
+            var controller = new TasksController(mockTastRepository.Object);
+
+            // Act
+            var result = await controller.GetTaskById(task.Id);
+
+            // Assert
+            var actionResult = Assert.IsType<OkObjectResult>(result.Result);
+            var model = Assert.IsType<TaskModel>(actionResult.Value);
+            Assert.Equal(task, model);
+        }
     }
 }
